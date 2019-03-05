3b76248edffb545020a0a4e1718fe6634cc5d96e
shintaro-agatsuma <shintaro-agatsuma@vcube.co.jp>
2019-03-05 11:06:08 +0900

    epsonデバイス制御ライブラリを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/epos-2.9.0.js') }}" defer></script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
